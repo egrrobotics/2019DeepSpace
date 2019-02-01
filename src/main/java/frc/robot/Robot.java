6b0f2cfa764264f08f9aa336f15cca997d4ed33a
Ludwant <antonludwig01@yahoo.com>
2019-02-01 12:59:32 -0500

    Added PID, Acceleration, Sensor subsystem, and basic auto commands

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Sensors;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -27,6 +28,7 @@ public class Robot extends TimedRobot {
   public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
   public static OI oi;
   public static DriveTrain driveTrain = new DriveTrain();
+  public static Sensors sensors = new Sensors();
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
---
+++ src/main/java/frc/robot/utils/PID.java
@@ -0,0 +1,28 @@
+package frc.robot.utils;
+
+public class PID {
+	public double kp, ki, kd, target;
+	public double lastError = 0;
+	public double totalError = 0;
+	public double error = 0;
+	public PID(double kp, double ki, double kd) {
+		this.kp = kp;
+		this.ki = ki;
+		this.kd = kd;
+	}
+	
+	public void setTarget(double target) {
+		this.target = target;
+	}
+	
+	public double getCorrection(double current) {
+		error = target - current;
+		totalError = 0.8*totalError + error;
+		//if (lastError * error <= 0) totalError = 0;
+		double changeInError = error - lastError;
+		double correction = kp * error + ki * totalError + kd * changeInError;
+		lastError = error;
+		return correction;
+	}
+
+}
